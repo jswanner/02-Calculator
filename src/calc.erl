30f315c8fd8280831c23fd0910f26988d82656fc
Jacob Swanner <swanner@gmail.com>
2010-02-05 10:49:35 -0500

    renamed Sum to Total for consistency. Added comments describing the flow of the logic

--- src/calc.erl
+++ src/calc.erl
@@ -2,14 +2,22 @@
 -export([add/1, sub/1, multiply/1, divide/1]).
 
 %% addition
+%% the same pattern is used for each type of math operation:
+%% the function is called with 1 argument, which is a list of numbers,
+%% the head of the list is pop'ed off and is the initial total
+%% the initial total and the rest of the list will be handled below
 add([H|T]) ->
   add(H, T).
 
-add(Sum, [H|T]) ->
-  add(Sum + H, T);
+%% the next head of the list is pop'ed off,
+%% that value is then used to modify the running total
+%% this process repeats until the list is empty
+add(Total, [H|T]) ->
+  add(Total + H, T);
 
-add(Sum, []) ->
-  Sum.
+%% once the list is empty, we just need to return our total
+add(Total, []) ->
+  Total.
 
 %% subtraction
 sub([H|T]) ->
